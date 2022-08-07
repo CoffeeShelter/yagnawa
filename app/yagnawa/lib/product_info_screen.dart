1648dc566f93a1e6b5d8c4ad90a5a2a1644351a5
CoffeeShelter <ij238950@gmail.com>
2022-08-08 00:52:57 +0900

    [feat]: 상품 정보 화면

--- app/yagnawa/lib/product_info_screen.dart
+++ app/yagnawa/lib/product_info_screen.dart
@@ -8,82 +8,91 @@ class ProductInfoScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Column(
-      children: <Widget>[
-        Container(
-          height: size.height * 0.4,
-          decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.circular(20),
+    return Container(
+      color: Colors.black,
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: size.height * 0.4,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
           ),
-        ),
-        Container(
-          alignment: Alignment.topLeft,
-          padding: const EdgeInsets.all(5.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Text(
-                '고려은단 비타민C 1000 + 비타민D',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+          Container(
+            alignment: Alignment.topLeft,
+            margin: const EdgeInsets.only(
+              top: 3,
+            ),
+            padding: const EdgeInsets.all(5.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  '고려은단 비타민C 1000 + 비타민D',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const Text(
+                  '고려은단 헬스케어(주)',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
+                  ),
+                ),
+                const Divider(
                   color: Colors.black,
+                  thickness: 1.5,
                 ),
-              ),
-              const Text(
-                '고려은단 헬스케어(주)',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black54,
-                ),
-              ),
-              const Divider(
-                color: Colors.black,
-                thickness: 1.5,
-              ),
-              Row(
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.only(right: 5),
-                    child: const Icon(Icons.check),
-                  ),
-                  const Text(
-                    '인증 현황',
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.black,
+                Row(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.only(right: 5),
+                      child: const Icon(Icons.check),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    width: 32,
-                    height: 32,
-                    margin: const EdgeInsets.only(top: 5),
-                    child: const Icon(Icons.ac_unit),
-                  ),
-                  Container(
-                    width: 32,
-                    height: 32,
-                    margin: const EdgeInsets.only(top: 5),
-                    child: const Icon(Icons.add_a_photo_outlined),
-                  ),
-                ],
-              )
-            ],
-          ),
-        )
-      ],
+                    const Text(
+                      '인증 현황',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      width: 32,
+                      height: 32,
+                      margin: const EdgeInsets.only(top: 5),
+                      child: const Icon(Icons.ac_unit),
+                    ),
+                    Container(
+                      width: 32,
+                      height: 32,
+                      margin: const EdgeInsets.only(top: 5),
+                      child: const Icon(Icons.add_a_photo_outlined),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
